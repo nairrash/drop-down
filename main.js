a7e159a1c0586d21304474c6a43cef9596200a93
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2016-08-16 22:56:20 -0700

    updating the lib

--- main.js
+++ main.js
@@ -18,38 +18,47 @@
       Autofill.prototype.showHelp = function(){
 //          console.log("rashmi");
           this.help.style.display = "block"
-      }
+      };
        Autofill.prototype.hideHelp = function(){
           this.help.style.display = "none"
-      }
+      };
       Autofill.prototype.setCountry = function(country){
           this.field.value = country;
           this.hideHelp();
-      }
+      };
+    Autofill.prototype.updateAutofill = function(context,e){
+      console.log("wow");
+      // var self = this;
+      // context.setCountry(self.innerHTM);
+      console.log(context);
+      console.log(e);
+      context.setCountry(e.target.innerHTML)
+    };
 
-      
-      Autofill.prototype.detectInput = function(){
-          var list = "";
-        var self = this;
-          var currValue = this.field.value;
-          console.log(this);
-          if(currValue.length >= 2) {//min characters
-//              this.getDropdown(self);
-              for(var i = 0; i < countryList.length; i++) {
-                  if(currValue.toLowerCase() === countryList[i].substr(0,currValue.length).toLowerCase()){
-                      if(countryList){
-                          console.log(auto);
-//                       
-				list += '<li>' + countryList[i] + '</li>'
-                         this.help.innerHTML = list;
-                          console.log(list);
-                      }
-//                     
-                 } 
-              }
+    Autofill.prototype.detectInput = function(){
+      var list = "";
+      var self = this;
+      var currValue = this.field.value;
+      if(currValue.length >= 2) {//min characters
+         for(var i = 0; i < countryList.length; i++) {
+           if(currValue.toLowerCase() === countryList[i].substr(0,currValue.length).toLowerCase()){
+             if(countryList){
+               list += '<li>' + countryList[i] + '</li>'
+               this.help.innerHTML = list;
+             }
+           }
+         }
+        this.help.childNodes.forEach(function(node){
+          // console.log(this);
+          node.addEventListener("click",self.updateAutofill.bind(this,self));
+        });
+        // console.log(this.help.childNodes);
+
           } 
           
       };
+      
+ 
       
    
       
